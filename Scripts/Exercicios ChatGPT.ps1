5250b44146433743c704f4e93ba0b99af09b0bc9
Mack <pedrogabrielsilva.d.s@gmail.com>
2024-10-17 17:54:09 -0300

    Scripts PowerShell

---
+++ Scripts/Exercicios ChatGPT.ps1
@@ -0,0 +1,18 @@
+﻿#Exercicios ChatGPT
+
+#Exercicio 1:
+#clear 
+#$Comprimento = New-Object -ComObject WScript.shell
+
+#$Comprimento.Popup("Olá PowerShell!")
+
+"------------------------------------------------------"
+
+#Exercicio 2:
+cls
+
+$Nome = Read-Host("Qual seu nome?")
+
+$BemVindo = ("Bem vindo ao PowerShell, $Nome!")
+
+$BemVindo 
---
+++ Scripts/Script Impressoras.ps1
@@ -0,0 +1,24 @@
+﻿#Script verificação de nomes das impressoras!
+
+$ListaDeImpressoras = Get-Content "C:\temp\Nomes_Impressoras.txt"
+
+$Padrao = "^IMP\d{6}$"
+
+$Padrao2 = "^ETIQ\d{6}$"
+
+#"^IMP\d{6}(-[A-Za-z0-9])$"
+
+
+ForEach ($Impressoras in $ListaDeImpressoras){
+    If($Impressoras -match $Padrao -or $Impressoras -match $Padrao2){
+
+       $Script = "`n $Impressoras (Impressora dentro do Padrão!!)"
+       Add-Content -Path "C:\Temp\Script.txt" -value $Script
+
+    }Else{
+       $Script2 = "`n $Impressoras (Impressora fora do Padrão!!)" 
+       Add-Content -Path "C:\Temp\Script2.txt" -value $Script2
+    }
+}
+
+
