b8e4d4d5019585be77d91b316f375e4b8413e65c
Mack <pedrogabrielsilva.d.s@gmail.com>
2024-10-28 17:32:59 -0300

    Log no PowerShell

---
+++ Scripts/Criando Log.ps1
@@ -0,0 +1,18 @@
+﻿#Criando um LOG
+
+function Write-CMTraceLog {
+    param (
+        [string]$LogPath,     # Caminho do arquivo de log
+        [string]$LogMessage,    # Mensagem de log a ser gravada
+        [string]$LogType = "INFO" # Tipo de log (INFO, ERROR, WARNING)
+    )
+
+    # Pega a data e hora atual no formato correto para o CMTrace
+    $timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss.fff"
+
+    # Formata a mensagem no estilo padrão do CMTrace
+    $formattedMessage = "$timeStamp [$LogType] $LogMessage"
+
+    # Adiciona a mensagem ao arquivo de log
+    Add-Content -Path $LogPath -Value $formattedMessage
+}
--- Scripts/Script Impressoras/Script Impressoras.ps1
+++ Scripts/Script Impressoras/Script Impressoras.ps1
@@ -4,25 +4,42 @@ $ListaDeImpressoras = Get-Content 'C:\Temp\Nomes_Impressoras.txt'
 
 $Padrao = '^(IMP|ETIQ)\d{6}(-\w+)?$'
 
+
+function EscreveLog {
+    param (
+        [string]$LogPath = "C:\Temp\Logs\meulog.log",
+        [string]$LogMessage = "Impressora fora do padrão!",
+        [string]$LogType = "INFO" 
+    )
+
+    $timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss.fff"
+
+    $formattedMessage = "$timeStamp [$LogType] $LogMessage"
+
+    Add-Content -Path $LogPath -Value $formattedMessage
+}
+
 WorkFlow Iniciar-Notas {
 
   Start-Process 'C:\Temp\Impressoras_Corretas.txt'
   Sleep 2
   Start-Process 'C:\Temp\Impressoras_Incorretas.txt'
+  Sleep 1
+  Start-Process 'C:\Temp\Logs\meulog.log'
 }
 
 ForEach ($Impressoras in $ListaDeImpressoras){
-    Foreach ($teste in $Padrao){
-        If($Impressoras -match $teste){
+       If($Impressoras -match $Padrao){
             $Script = "`n $Impressoras"
             Add-Content -Path "C:\Temp\Impressoras_Corretas.txt" -value $Script   
-        }Else{
+       }Else{
             $Script2 = "`n $Impressoras" 
             Add-Content -Path "C:\Temp\Impressoras_Incorretas.txt" -value $Script2
-        }
-    }
+            EscreveLog
+       }
 }
 Iniciar-Notas
+
 
 
 
