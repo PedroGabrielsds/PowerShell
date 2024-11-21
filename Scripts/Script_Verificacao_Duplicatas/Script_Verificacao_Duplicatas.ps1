9945ba1c2268f6f07831606a7fca14cc1aff355c
Mack <pedrogabrielsilva.d.s@gmail.com>
2024-11-21 17:26:55 -0300

    Script Verificação Duplicatas

---
+++ Scripts/Script_Verificacao_Duplicatas/Script_Verificacao_Duplicatas.ps1
@@ -0,0 +1,47 @@
+﻿#Script conferência de máquinas duplicadas
+#===========================================
+#Variaveis
+$OU = "OU=Estacoes,OU=Microinformatica,OU=Maquinas,DC=rede,DC=stf,DC=gov,DC=br"
+
+$ETU_ETI = @()
+
+#===========================================
+#Importando módulo AD
+
+Try {
+    Import-Module ActiveDirectory 
+    Write-Host "Módulo Importado com sucesso..."
+} Catch {
+    Write-Host "Ocorreu um erro ao importar o módulo AD!!"
+}
+
+
+$Computadores = @()
+
+$MaquinasAD = Get-ADComputer -Filter * -SearchBase $OU -Properties CN
+
+ForEach($Maquina in $MaquinasAD){
+    $Computadores += [PSCustomObject] @{
+        CN = ($Maquina.CN -Split ',',1)[0]
+    
+        }
+}
+
+
+#===========================================
+#InicioAlgoritimo 
+ 
+ForEach($Computador in $Computadores){
+    If(($Computador.CN -match "ETU") -or ($Computador.CN -match "ETI")){
+        $ETU_ETI = [PsCustomObject] @{
+            CN = $($Computador.CN)
+        }
+    }
+}
+
+
+
+
+
+#FimAlgoritimo
+#=============================================
