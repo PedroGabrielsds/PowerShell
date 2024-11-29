91c67823275c3f6106e318c925d17646625c230a
Mack <pedrogabrielsilva.d.s@gmail.com>
2024-11-29 17:06:42 -0300

    Exercicios ChatGPT v2.0 (ex:9)

--- Scripts/Exercicios PowerShell ChatGPT/Exercicios ChatGPT 2.0.ps1
+++ Scripts/Exercicios PowerShell ChatGPT/Exercicios ChatGPT 2.0.ps1
@@ -1,6 +1,6 @@
 ﻿#Exercicios ChatGPT
-#==============================================================
-#Exercicio 1: 
+#========================================================================================================
+#Exercicio 8: 
 
 #$Endereço = 'C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Exercicios PowerShell ChatGPT\Resultado_Ex1.txt'
 
@@ -29,12 +29,62 @@
   #  Pesquisa_Processo
 #}
 
-#==============================================================
-#Exercicio 2: 
-Get-ComputerInfo | Select-Object CsName, WindowsVersion, WindowsProductName
+#========================================================================================================
+#Exercicio 9: 
+
+#$Info_Computador = Get-ComputerInfo |Select-Object CsName, WindowsProductName, WindowsVersion
+
+#$Espaco = (Get-Volume -DriveLetter C | Select-Object @{Name="Espaco_Disponivel"; Expression={[Math]::Round($_.SizeRemaining / 1GB, 2)}}).Espaco_Disponivel
+
+#$Resultado = @{
+
+ #   "Nome do computador: "             = $Info_Computador.CsName
+
+  #  "Versão do S.O: "  = $Info_Computador.WindowsVersion
+
+   # "Espaço disponivel em C: "         = "$Espaco GB"
+
+#}
+
+#$Resultado 
+
+#=========================================================================================================
 
 
 
 
 
-#==============================================================
+
+
+
+#--------------------------------------------------------------------------------------------------------
+#Comandos de buscar informações do computador:
+
+#Get-ComputerInfo | Select-Object CsName, WindowsVersion, WindowsProductName
+
+#Get-PSDrive
+
+#Get-WmiObject -Class Win32_Processor
+
+#--------------------------------------------------------------------------------------------------------
+#Metodos de buscar informações de armazenamento:
+
+#Get-PhysicalDisk
+
+#Get-Partition
+
+#Get-Disk | 
+
+#Get-Volume | Select-Object @{Name="Espaço_Livre"; Expression={[Math]::Round($_.SizeRemaining / 1GB, 2)}}, @{Name="Espaço"; Expression={[Math]::Round($_.Size / 1GB, 3)}}
+
+#Get-Disk | Select-Object FriendlyName, @{Name="SizeGB"; Expression={[math]::round($_.Size / 1GB, 2)}}
+
+#--------------------------------------------------------------------------------------------------------
+
+
+#----------------------------------------
+#Expressão de arredondamento
+#[Math]::Round(1.4)
+#----------------------------------------
+
+
