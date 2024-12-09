2ada852d34720c0d145d4ed1059f6ee5045a4d6e
Mack <pedrogabrielsilva.d.s@gmail.com>
2024-12-09 16:01:05 -0300

    Exercicio 17 - chatGPT v3.0

--- Scripts/Exercicios PowerShell ChatGPT/Exercicios ChatGPT  v3.0.ps1
+++ Scripts/Exercicios PowerShell ChatGPT/Exercicios ChatGPT  v3.0.ps1
@@ -3,53 +3,61 @@
 #Exercico 17: 
 #Criando CSV
 
-Function Coleta {
-    Param($Nome, $Email)
-    $Nome = Read-Host "Seu nome: "
+$arquivoCsv = 'C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Exercicios PowerShell ChatGPT'
 
-    $Email = Read-Host "Seu E-mail: "
+New-Item -Path $arquivoCsv -Name "Banco_Dados.csv"
 
-}
+cls
 
 Write-Host "|========================|" -ForegroundColor Yellow -BackgroundColor Black
 Write-Host "|    Banco de Dados      |" -ForegroundColor Yellow -BackgroundColor Black
 Write-Host "|========================|" -ForegroundColor Yellow -BackgroundColor Black
 
-cls
-
-$arquivoCsv = 'C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Exercicios PowerShell ChatGPT\'
-
-New-Item -Path $arquivoCsv -Name "Banco_Dados.csv"
+$Email_Correto = '^[a-z0-9+]+@[a-z0-9.-]+\.[a-z]{2,}$'
 
 $Dados = @()
 
-$Dados += [PSCustomObject]@{
+$Nome = Read-Host "Seu nome: "
+
+$Email = Read-Host "Seu E-mail: "
+
+If ($Email -match $Email_Correto) {
+
+    $Dados += [PSCustomObject] @{
     Nome = $Nome
     Email = $Email
-    
+    }
+    Clear-Host
+} Else {
+    Write-Host "O e-mail digitado está incorreto! " -ForegroundColor Red -BackgroundColor Black
+
+
 }
 
 $Bem_Vindo = "Seja bem vindo, $Nome!"
 ForEach ($Letra in $Bem_Vindo.ToCharArray()) {
-    Write-Host $Letra -NoNewline -ForegroundColor Red
+    Write-Host $Letra -NoNewline -ForegroundColor Green
     Start-Sleep -Milliseconds 100
 
 }
 
-$Resposta = Read-Host "Para exportar em um csv digite (Sim) e (Não) para cancelar!"
+Write-Host
+$Resposta = Read-Host "Exportar em um csv? Sim/Não"
 
-Try {
-    $Banco_Dados | Export-Csv -Path $arquivoCsv -NoTypeInformation -Delimiter ";"
+If(($Resposta -eq "Sim") -or ($Resposta -eq "S")){
+    
+    Try {
+        
+        $Dados | Export-Csv -Path $arquivoCsv -NoTypeInformation -Delimiter ";" 
 
-} Catch {
-    Write-Host "Erro ao exportar para o arquivo.csv"
+    } Catch {
+        Write-Host "Erro ao exportar para o arquivo.csv"
+
+    }
+} Else {
+
+    Write-Host "A operação foi cancelada!" -ForeGroundColor Red -BackGroundColor DarkBlue
 
 }
-
-
-
-
-
-
 
 #=================================================================
