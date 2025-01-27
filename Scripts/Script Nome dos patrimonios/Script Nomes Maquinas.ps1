5d5da52e091ac0d2e548ac90de350f16ad662dbd
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-01-27 17:09:25 -0300

    Update Script Maquinas AD

--- Scripts/Script Nome dos patrimonios/Script Nomes Maquinas.ps1
+++ Scripts/Script Nome dos patrimonios/Script Nomes Maquinas.ps1
@@ -3,16 +3,13 @@
 #Variaveis
 $OU = "OU=Estacoes,OU=Microinformatica,OU=Maquinas,DC=rede,DC=stf,DC=gov,DC=br"
 
-$Entrada = "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Script Nome dos patrimonios\Identificação dos patrimonios\Planilha_Caseiro.txt"
+$Entrada = "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Script Nome dos patrimonios\Maquinas_Doacao.txt"
 
-$Saida = "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Script Nome dos patrimonios\Identificação dos patrimonios\Maquinas_Identificadas.txt"
-
-$AD = "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Script Nome dos patrimonios\Identificação dos patrimonios\Patrimonios_AD.txt"
+$Saida = "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Script Nome dos patrimonios\Maquinas_Identificadas.txt"
 
 $Computadores = @()
 
-Add-Content -Path $AD -Value $Computadores
- 
+Add-Content -Path "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Script Nome dos patrimonios\Patrimonios_AD.txt" -Value $($Computadores)
 $Patrimonios = Get-Content $Entrada
 
 #==============================================================
@@ -35,11 +32,11 @@ ForEach($Maquina in $MaquinasAD){
 ForEach($Computador in $Computadores){
 
     ForEach($Patrimonio in $Patrimonios){
-
-        Write-Host "$($Patrimonio) X $($Computador.cn)"
-        If($Computador -match $Patrimonio){
+        
+        #Write-Host "$($Patrimonio) X $($Computador.cn)"
+        If($Computador -contains $Patrimonio){
             #Add-Content -path $saida -Value "$($Computador.CN)"
-            #Write-Host $Computador.CN
+            Write-Host $($Computador.CN)
         }
     }
 }
