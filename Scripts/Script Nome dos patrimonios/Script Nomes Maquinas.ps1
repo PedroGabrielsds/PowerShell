b9bbbb4db7a8ea09f8f8986e28182f7c61c14eff
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-01-27 17:22:07 -0300

    Update Script Maquinas Doação

--- Scripts/Script Nome dos patrimonios/Script Nomes Maquinas.ps1
+++ Scripts/Script Nome dos patrimonios/Script Nomes Maquinas.ps1
@@ -10,6 +10,7 @@ $Saida = "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scr
 $Computadores = @()
 
 Add-Content -Path "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Script Nome dos patrimonios\Patrimonios_AD.txt" -Value $($Computadores)
+
 $Patrimonios = Get-Content $Entrada
 
 #==============================================================
@@ -21,7 +22,8 @@ $MaquinasAD = Get-ADComputer -Filter * -SearchBase $OU -Properties CN
 
 ForEach($Maquina in $MaquinasAD){
     $Computadores += [PSCustomObject] @{
-        CN = ($Maquina.CN -Split ',',1)[0]
+        CN = ($Maquina.CN) 
+        #-Split ',',1)[0]
     
     }
 }
