ed8e9f9a69984e5f9b22d743a406a73ce2f498a6
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-01-29 17:46:56 -0300

    Update Maquinas Doaçao

---
+++ Scripts/Scripts STF/Script Excluida ou não/Verificação Maquinas Doação.ps1
@@ -0,0 +1,34 @@
+﻿#Script Verificação Maquinas Doação
+#=============================================================
+#Variaveis
+
+$Maquinas_Identificadas_AD = "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Script Maquinas Doação\Maquinas_Identificadas.txt"
+
+$Maquinas_Doadas = "C:\Users\g311011\Desktop\Pedro Gabriel Silva dos Santos\PowerShell\Scripts\Script Maquinas Doação\Maquinas_Doacao.txt"
+
+$Maquinas_Encontrada
+
+$Maquinas_Nao_Encontrada
+
+
+#==============================================================
+#InicioAlgoritmo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#==============================================================
+#FimAlgoritmo
